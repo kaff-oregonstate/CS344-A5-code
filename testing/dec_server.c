da972bf0390e3f2f45495fc892acad9b375eee53
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2021-03-06 01:39:47 -0800

    upload for clone

---
+++ testing/dec_server.c
@@ -0,0 +1,283 @@
+// dec_server.c
+
+// https://stackoverflow.com/questions/1568786/fork-before-or-after-accepting-connections
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+// Error function used for reporting issues
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address,
+                        int portNumber){
+
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address));
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+  // Allow a client at any address to connect to this server
+  address->sin_addr.s_addr = INADDR_ANY;
+}
+
+char decode_char(char msg, char key) {
+    if (msg == '\n') return '\n';
+    if (msg == '\0') return '\0';
+    int msg_int;
+    int key_int;
+    if (msg == ' ') msg_int = 0;
+    else msg_int = msg - 64;
+    // printf("%c = %d\n", msg, msg_int);
+    if (key == ' ') key_int = 0;
+    else key_int = key - 64;
+    // printf("%c = %d\n", key, key_int);
+    msg_int = (msg_int - key_int) % 27;
+    if (msg_int < 0) msg_int = msg_int + 27;
+    if (msg_int == 0) return ' ';
+    else return msg_int + 64;
+}
+
+
+int main(int argc, char *argv[]){
+  int connectionSocket, charsRead, newLine;
+  char buffer_txt[256];
+  char buffer_key[256];
+  char next_char;
+  struct sockaddr_in serverAddress, clientAddress;
+  socklen_t sizeOfClientInfo = sizeof(clientAddress);
+
+  // set handshake messages
+  char dec_handshake_client[36] = "I am requesting decoding service.\n\0";
+  char dec_handshake_server[25] = "Proceed with decoding.\n\0";
+
+  // Check usage & args
+  if (argc < 2) {
+    fprintf(stderr,"USAGE: %s port\n", argv[0]);
+    exit(1);
+  }
+
+  // Create the socket that will listen for connections
+  int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenSocket < 0) {
+    error("ERROR opening socket");
+  }
+
+  // Set up the address struct for the server socket
+  setupAddressStruct(&serverAddress, atoi(argv[1]));
+
+  // Associate the socket to the port
+  if (bind(listenSocket,
+          (struct sockaddr *)&serverAddress,
+          sizeof(serverAddress)) < 0){
+    error("ERROR on binding");
+  }
+
+  // Start listening for connections. Allow up to 5 connections to queue up
+  listen(listenSocket, 5);
+
+  int number_of_children = 0;
+
+  for (size_t j = 0; j < 4; j++) {
+      pid_t spawnPid = fork();
+      switch (spawnPid) {
+          case -1:
+              perror("fork()\n");
+              exit(1);
+              break;
+          case 0:
+              // child process instructions
+              // Accept a connection, blocking if one is not available until one connects
+              while(1){
+
+// COPY HERE
+
+                  // Accept the connection request which creates a connection socket
+                  connectionSocket = accept(listenSocket,
+                      (struct sockaddr *)&clientAddress,
+                      &sizeOfClientInfo);
+
+                  if (connectionSocket < 0){
+                      error("ERROR on accept");
+                  }
+
+                  // printf("SERVER: Connected to client running at host %d port %d\n",
+                  //     ntohs(clientAddress.sin_addr.s_addr),
+                  //     ntohs(clientAddress.sin_port));
+
+                  // Get the message from the client and check it
+                  memset(buffer_txt, '\0', sizeof(buffer_txt));
+                  // Read the client's message from the socket
+                  charsRead = recv(connectionSocket, buffer_txt, sizeof(buffer_txt) - 1, 0);
+                  if (charsRead < 0) error("ERROR reading from socket 4");
+
+                  if (strcmp(dec_handshake_client, buffer_txt) == 0) {
+                      // offer decoding service
+                      charsRead = send(connectionSocket,
+                          dec_handshake_server, sizeof(dec_handshake_server), 0);
+                      if (charsRead < 0){
+                          error("ERROR writing to socket");
+                      }
+
+// LOOP HERE: MSG THEN KEY, decODE, RETURN
+
+// loop to get, decode, send until data stream complete
+newLine = 0;
+while (!newLine) {
+    // Get the next message datagram from the client
+    memset(buffer_txt, '\0', sizeof(buffer_txt));
+    charsRead = recv(connectionSocket, buffer_txt, sizeof(buffer_txt) - 1, 0);
+    if (charsRead < 0) error("ERROR reading from socket 1");
+
+    charsRead = send(connectionSocket,
+        dec_handshake_server, strlen(dec_handshake_server), 0);
+    if (charsRead < 0){
+        error("ERROR writing to socket");
+    }
+
+    // Get the next key datagram from the client
+    memset(buffer_key, '\0', sizeof(buffer_key));
+    charsRead = recv(connectionSocket, buffer_key, sizeof(buffer_key) - 1, 0);
+    if (charsRead < 0) error("ERROR reading from socket 2");
+
+    // printf("%s\n", buffer_txt);
+    // printf("%s\n", buffer_key);
+
+    // alg on chars (strlen(buffer_txt))
+    for (size_t l = 0; l < strlen(buffer_txt); l++) {
+        next_char = decode_char(buffer_txt[l], buffer_key[l]);
+        if (next_char == '\n') newLine = 1;
+        buffer_txt[l] = next_char;
+    }
+
+    // send compiled, decoded msg
+    charsRead = send(connectionSocket,
+        buffer_txt, sizeof(buffer_txt) - 1, 0);
+    if (charsRead < 0) error("ERROR writing to socket");
+}
+                  }
+                  else {
+                      // gtfoh
+                      // printf("bruh.\n");
+                      charsRead = send(connectionSocket,
+                          "git the foo outta he'e", 22, 0);
+                      if (charsRead < 0){
+                          error("ERROR writing to socket");
+                      }
+                  }
+
+// TO HERE
+
+                  // Close the connection socket for this client
+                  close(connectionSocket);
+              }
+
+              exit(1);
+              break;
+          default:
+              // increment numOfChildren
+              number_of_children = number_of_children + 1;
+              if (number_of_children == 4) {
+                  // Accept a connection, blocking if one is not available until one connects
+                  while(1){
+
+// COPY HERE
+
+                // Accept the connection request which creates a connection socket
+                connectionSocket = accept(listenSocket,
+                    (struct sockaddr *)&clientAddress,
+                    &sizeOfClientInfo);
+
+                if (connectionSocket < 0){
+                    error("ERROR on accept");
+                }
+
+                // printf("SERVER: Connected to client running at host %d port %d\n",
+                //     ntohs(clientAddress.sin_addr.s_addr),
+                //     ntohs(clientAddress.sin_port));
+
+                // Get the message from the client and check it
+                memset(buffer_txt, '\0', sizeof(buffer_txt));
+                // Read the client's message from the socket
+                charsRead = recv(connectionSocket, buffer_txt, sizeof(buffer_txt) - 1, 0);
+                if (charsRead < 0) error("ERROR reading from socket 4");
+
+                if (strcmp(dec_handshake_client, buffer_txt) == 0) {
+                    // offer decoding service
+                    charsRead = send(connectionSocket,
+                        dec_handshake_server, sizeof(dec_handshake_server), 0);
+                    if (charsRead < 0){
+                        error("ERROR writing to socket");
+                    }
+
+// LOOP HERE: MSG THEN KEY, decODE, RETURN
+
+// loop to get, decode, send until data stream complete
+newLine = 0;
+while (!newLine) {
+  // Get the next message datagram from the client
+  memset(buffer_txt, '\0', sizeof(buffer_txt));
+  charsRead = recv(connectionSocket, buffer_txt, sizeof(buffer_txt) - 1, 0);
+  if (charsRead < 0) error("ERROR reading from socket 1");
+
+  charsRead = send(connectionSocket,
+      dec_handshake_server, strlen(dec_handshake_server) + 1, 0);
+  if (charsRead < 0){
+      error("ERROR writing to socket");
+  }
+
+  // Get the next key datagram from the client
+  memset(buffer_key, '\0', sizeof(buffer_key));
+  charsRead = recv(connectionSocket, buffer_key, sizeof(buffer_key) - 1, 0);
+  if (charsRead < 0) error("ERROR reading from socket 2");
+
+  // printf("%s\n", buffer_txt);
+  // printf("%s\n", buffer_key);
+
+  // alg on chars (strlen(buffer_txt))
+  for (size_t l = 0; l < strlen(buffer_txt); l++) {
+      next_char = decode_char(buffer_txt[l], buffer_key[l]);
+      if (next_char == '\n') newLine = 1;
+      buffer_txt[l] = next_char;
+  }
+
+  // send compiled, decoded msg
+  charsRead = send(connectionSocket,
+      buffer_txt, sizeof(buffer_txt) - 1, 0);
+  if (charsRead < 0) error("ERROR writing to socket");
+}
+                }
+                else {
+                    // gtfoh
+                    // printf("bruh.\n");
+                    charsRead = send(connectionSocket,
+                        "git the foo outta he'e", 22, 0);
+                    if (charsRead < 0){
+                        error("ERROR writing to socket");
+                    }
+                }
+
+// TO HERE
+
+                      // Close the connection socket for this client
+                      close(connectionSocket);
+                  }
+              }
+              break;
+      }
+  }
+
+  // Close the listening socket
+  close(listenSocket);
+  return 0;
+}
---
+++ testing/enc_client.c
@@ -0,0 +1,185 @@
+// enc_client.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+
+#include <fcntl.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <math.h>
+
+/**
+* Client code
+* 1. Create a socket and connect to the server specified in the command arugments.
+* 2. Prompt the user for input and send that input as a message to the server.
+* 3. Print the message received from the server and exit the program.
+*/
+
+// Error function used for reporting issues
+void error(const char *msg) {
+  perror(msg);
+  exit(0);
+}
+
+// Set up the address struct
+void setupAddressStruct(struct sockaddr_in* address,
+                        int portNumber,
+                        char* hostname){
+
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address));
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+
+  // Get the DNS entry for this host name
+  struct hostent* hostInfo = gethostbyname(hostname);
+  if (hostInfo == NULL) {
+    fprintf(stderr, "CLIENT: ERROR, no such host\n");
+    exit(0);
+  }
+  // Copy the first IP address from the DNS entry to sin_addr.s_addr
+  memcpy((char*) &address->sin_addr.s_addr,
+        hostInfo->h_addr_list[0],
+        hostInfo->h_length);
+}
+
+int main(int argc, char *argv[]) {
+  int socketFD, portNumber, charsWritten, charsRead, charsChecked, charsReadTotal, badNewLine;
+  struct sockaddr_in serverAddress;
+  char buffer_txt[256];
+  char checker[256];
+  char buffer_key[256];
+
+  // set handshake messages
+  char enc_handshake_client[36] = "I am requesting encoding service.\n\0";
+  char enc_handshake_server[25] = "Proceed with encoding.\n\0";
+
+  // Check usage & args
+  if (argc < 4) {
+    fprintf(stderr,"USAGE: %s plaintext key port\n", argv[0]);
+    exit(0);
+  }
+
+  // open file descriptors for plaintext and key
+  int plaintext_fd = open(argv[1], O_RDONLY);
+  int key_fd = open(argv[2], O_RDONLY);
+
+  // check file lengths
+  struct stat stat1, stat2;
+  fstat(plaintext_fd, &stat1);
+  fstat(key_fd, &stat2);
+  // printf("%lld\n", stat1.st_size);
+  // printf("%lld\n", stat2.st_size);
+  if (stat1.st_size > stat2.st_size) {
+      fprintf(stderr, "CLIENT: key \'%s\' is too short\n", argv[2]);
+      exit(1);
+  }
+
+  // Create a socket
+  socketFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketFD < 0){
+    error("CLIENT: ERROR opening socket");
+  }
+
+   // Set up the server address struct
+  setupAddressStruct(&serverAddress, atoi(argv[3]), "localhost");
+
+  // Connect to server
+  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+    error("CLIENT: ERROR connecting");
+  }
+
+  // Send message to server
+  // Write to the server
+  charsWritten = send(socketFD, enc_handshake_client, strlen(enc_handshake_client), 0);
+  if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+  if (charsWritten < strlen(enc_handshake_client)){
+    fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+  }
+
+  // Get return message from server
+      // Clear out the buffer_txt again for reuse
+      memset(buffer_txt, '\0', sizeof(buffer_txt));
+  // Read data from the socket, leaving \0 at end
+  charsRead = recv(socketFD, buffer_txt, sizeof(buffer_txt) - 1, 0);
+  if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+
+  if (strcmp(enc_handshake_server, buffer_txt) == 0) {
+      // do encoding
+
+      // read key
+      memset(buffer_key, '\0', sizeof(buffer_key));
+      charsRead = read(key_fd, buffer_key, sizeof(buffer_key) - 2);
+      if (charsRead < 0) error("CLIENT: ERROR reading from key");
+
+      // read plaintext
+      memset(buffer_txt, '\0', sizeof(buffer_txt));
+      charsRead = read(plaintext_fd, buffer_txt, sizeof(buffer_txt) - 2);
+      charsReadTotal = charsRead;
+
+      // loop check, send, recieve, print, read until read returns 0
+      while (charsRead > 0) {
+          // check for input validity
+          memset(checker, '\0', sizeof(checker));
+          charsChecked = pread(plaintext_fd, checker, sizeof(checker) - 1, charsReadTotal);
+          if (checker[0] == '\0') badNewLine = 0;
+          else badNewLine = 1;
+          int next_char;
+          for (size_t k = 0; k < strlen(buffer_txt); k++) {
+              next_char = buffer_txt[k];
+              if (next_char == 32) continue;
+              if (next_char > 64 && next_char < 91) continue;
+              if (next_char == 10) {
+                  if (!badNewLine && k == strlen(buffer_txt) - 1) continue;
+              }
+              error("CLIENT: bad input char");
+          }
+
+          // send MSG
+          // if (strlen(buffer_txt) == 256)
+          charsWritten = send(socketFD, buffer_txt, strlen(buffer_txt), 0);
+          if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+
+          memset(buffer_txt, '\0', sizeof(buffer_txt));
+          charsRead = recv(socketFD, buffer_txt, sizeof(buffer_txt) - 1, 0);
+          if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+          if (strcmp(enc_handshake_server, buffer_txt) != 0) error("CLIENT: ERROR not given proceed directive");
+
+          // send KEY
+          charsWritten = send(socketFD, buffer_key, strlen(buffer_key), 0);
+          if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+
+          // get encoded msg
+                // *** may cause errors with '\0' at end due to recv
+          charsRead = recv(socketFD, buffer_txt, sizeof(buffer_txt) - 1, 0);
+          if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+
+          // print encoded msg
+          fprintf(stdout, "%s", buffer_txt);
+
+          // read next key data
+          memset(buffer_key, '\0', sizeof(buffer_key));
+          charsRead = read(key_fd, buffer_key, sizeof(buffer_key) - 1);
+          if (charsRead < 0) error("CLIENT: ERROR reading from key");
+          // read next plaintext data
+          memset(buffer_txt, '\0', sizeof(buffer_txt));
+          charsRead = read(plaintext_fd, buffer_txt, sizeof(buffer_txt) - 1);
+          charsReadTotal = charsReadTotal + charsRead;
+      }
+      if (charsRead < 0) error("CLIENT: ERROR reading from plaintext");
+  }
+
+  // Close the socket
+  close(socketFD);
+  return 0;
+}
